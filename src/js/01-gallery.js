4bfed973906fb045e1e8caac031b484a8700aab1
MaxKov86 <max.kov86@ukr.net>
2022-11-26 20:40:42 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,37 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(`.gallery`);
+const galleryCards = createGalleryCards(galleryItems);
+
+galleryList.insertAdjacentHTML(`beforeend`, galleryCards);
+
+galleryList.addEventListener(`click`, onGalleryCardClick)
+
+function createGalleryCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `
+    }).join(``);
+}
+
+function onGalleryCardClick(e) {
+    e.preventDefault()
+
+    if (!e.target.classList.contains(`gallery__image`)) {
+        return;
+    }
+}
+
+let galleryEl = new SimpleLightbox('.gallery a', {
+        captionsData: `alt`,
+        captionDelay: 250,
+});
+
+galleryEl.on('show.simplelightbox', function () {
+	
+});
