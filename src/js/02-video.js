c218b06cc91a0caffd86c01ab06b3d58c4581c01
MaxKov86 <max.kov86@ukr.net>
2022-11-26 20:40:49 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+
+const STORAGE_KEY = `vplayer-current-time`;
+
+const iframe = document.querySelector(`iframe`);
+console.log(iframe);
+
+
+const player = new Player(iframe, {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+});
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+
+player.on('timeupdate', throttle(onPlayUpdate, 1000));
+
+function onPlayUpdate (event) {
+    localStorage.setItem(STORAGE_KEY, event.seconds);
+}
 
